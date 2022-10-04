21c40f83f18dd897512cdbd5efbae4b3c881c952
ZadenMubarak <zaden.ngobeni@gmail.com>
2022-10-04 10:30:37 -0700

    Home page started

---
+++ loyalty_programme/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Middiv from './components/Middiv';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <span>How it works</span>
+          <span></span>
+        </ul>
+      </nav>
+      <Middiv/>
+    </div>
+  );
+}
+
+export default App;
---
+++ loyalty_programme/src/components/Middiv.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles/Middiv.css'
+
+const Middiv = () => {
+  return (
+    <div className='parent'>
+        <div className='route-card'>
+            <h1>What is your bussiness' industry</h1>
+            <br/><br/><br/>
+            
+            <button>services</button>
+        </div>
+    </div>
+  )
+}
+
+export default Middiv
